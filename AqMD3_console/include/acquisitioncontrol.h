7873299a6d53c76f5e42913f1e625ef04cf6fb07
Cameron Giberson <cameron.giberson@pnnl.gov>
2019-12-11 16:13:52 -0800

    change acquisition controllers interface

--- AqMD3_console/include/acquisitioncontrol.h
+++ AqMD3_console/include/acquisitioncontrol.h
@@ -1,15 +1,35 @@
 #ifndef ACQUISITION_CONTROL_H
 #define ACQUISITION_CONTROL_H
 
+#include <UIMFWriter/uimfframe.h>
 #include <iostream>
+#include <atomic>
 
 // High level interface for managing acquisition.
 class AcquisitionControl {
+protected:
+	enum class State : uint8_t
+	{
+		ACQUIRING,
+		STOPPED,
+		ERRORED,
+		UNKNOWN
+	};
+
+	std::atomic<State> state;
+
 public:
+	AcquisitionControl()
+		: state(State::UNKNOWN)
+	{}
+
 	virtual ~AcquisitionControl() = default;
 
+	virtual void start(std::shared_ptr<UimfFrame> frame) = 0;
 	virtual void start() = 0;
 	virtual void stop() = 0;
+
+	inline bool is_acquiring() { return state == State::ACQUIRING; }
 
 };
 
