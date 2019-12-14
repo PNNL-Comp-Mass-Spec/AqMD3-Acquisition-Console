10af41507f41ee3ba01377db2cfe96e4e50fd36b
Cameron Giberson <cameron.giberson@pnnl.gov>
2019-12-13 18:23:41 -0800

    update process subject to emit UimfFrame objects

--- AqMD3_console/include/processsubject.h
+++ AqMD3_console/include/processsubject.h
@@ -8,37 +8,51 @@
 #include "server.h"
 #include <zmq.hpp>
 
-class ProcessSubject : public FrameSubscriber<AcquiredData>, public FramePublisher<std::shared_ptr<std::vector<EncodedResult>>> {
+using segment_ptr = std::shared_ptr<std::vector<EncodedResult>>;
+using frame_ptr = std::shared_ptr<UimfFrame>;
+
+class ProcessSubject : public FrameSubscriber<AcquiredData>, 
+	public FramePublisher<segment_ptr>,
+	public FramePublisher<frame_ptr> {
 private:
-	std::shared_ptr<UimfFrame> frame;
-	int process_index;
 	int total_triggers_processed;
 	std::shared_ptr<Server::Publisher> publisher;
 	std::string subject;
-
 	std::chrono::milliseconds total_duration;
 	int total_elements_processed;
-
 	uint32_t offset_bins;
+	uint64_t tof_avg_samples;
+	std::deque<std::shared_ptr<UimfFrame>> frames;
 
 public:
-	ProcessSubject(std::shared_ptr<UimfFrame> frame, std::shared_ptr<Server::Publisher> publisher)
-		: frame(frame)
-		, process_index(0)
-		, total_triggers_processed(0)
+	ProcessSubject(std::shared_ptr<UimfFrame> frame, std::shared_ptr<Server::Publisher> publisher, uint64_t tof_avg_samples)
+		: total_triggers_processed(0)
 		, publisher(publisher)
 		, subject("status")
 		, total_duration(0)
 		, total_elements_processed(0)
 		, offset_bins(0)
-	{}
+		, tof_avg_samples(tof_avg_samples)
+	{
+		if (frame->nbr_accumulations == 1)
+		{
+			frames.push_back(frame->clone_at_frame(frame->frame_number));
+		}
+		else
+		{
+			for (int i = 0; i < frame->nbr_accumulations; i++)
+			{
+				frames.push_back(frame->clone_at_frame(i));
+			}
+		}
+	}
 
-	ProcessSubject(uint32_t offset_bins)
-		: process_index(0)
-		, total_triggers_processed(0)
+	ProcessSubject(uint32_t offset_bins, uint64_t tof_avg_samples)
+		: total_triggers_processed(0)
 		, total_elements_processed(0)
 		, total_duration(0)
 		, offset_bins(offset_bins)
+		, tof_avg_samples(tof_avg_samples)
 	{}
 
 private:
@@ -46,7 +60,7 @@ private:
 
 	void on_completed() override
 	{
-		notify_completed_and_wait();
+		FramePublisher<segment_ptr>::notify_completed_and_wait();
 	}
 };
 
--- AqMD3_console/src/processsubject.cpp
+++ AqMD3_console/src/processsubject.cpp
@@ -3,18 +3,16 @@
 static uint64_t ts_last = 0;
 static uint64_t ts_first = 0;
 
+static int delta = 100;
+
 void ProcessSubject::on_notify() 
 {
 	while (!items.empty())
 	{
-		std::cout << " ProcessSubject unprocessed elements: " << items.size() << std::endl;
+		//std::cout << " ProcessSubject unprocessed elements: " << items.size() << std::endl;
 
-		auto start_0 = std::chrono::high_resolution_clock::now();
 		auto ad = items.front();
-		items.pop_front();		
-		auto stop_0 = std::chrono::high_resolution_clock::now();
-		auto ms_0 = std::chrono::duration_cast<std::chrono::milliseconds>(stop_0 - start_0);
-		total_duration += ms_0;
+		items.pop_front();
 
 		ts_first = (ad.stamps.begin())->timestamp;
 		//std::cout << "ts diff: " << ts_first - ts_last << std::endl;
@@ -31,39 +29,49 @@ void ProcessSubject::on_notify()
 		}
 		avg_ts /= ad.stamps.size() - 1;
 
-		if (avg_ts > 200100)
+		if (avg_ts > (tof_avg_samples + delta))
 		{
 			std::cout << "timestamps size: " << ad.stamps.size() << std::endl;
 			std::cout << "timestamps avg: " << avg_ts << std::endl;
 		}
 
-		if (frame)
+		if (!frames.empty())
 		{
+			auto frame = frames.front();
+
 			int total_triggers = frame->frame_length * frame->nbr_accumulations;
 
 			if (total_triggers_processed < total_triggers)
-			{			
-				auto start_1 = std::chrono::high_resolution_clock::now();
+			{
 				auto results = ad.process(total_triggers_processed % frame->frame_length, frame->offset_bins);
 				total_triggers_processed += ad.stamps.size();
-				auto stop_1 = std::chrono::high_resolution_clock::now();
-				auto ms_1 = std::chrono::duration_cast<std::chrono::milliseconds>(stop_1 - start_1);
-				
-				total_duration += ms_1;
+
 				for (int i = 0; i < results->size(); i++)
 				{
 					total_elements_processed += results->at(i).non_zero_count;
 				}
-				
-				notify(results, SubscriberType::BOTH);
-				process_index = 0;
+
+				FramePublisher<segment_ptr>::notify(results, SubscriberType::BOTH);
+
+				auto excess = frame->append_and_return_excess(results);
+
+				if (frame->frame_length == frame->get_encoded_result_count())
+				{
+					FramePublisher<frame_ptr>::notify(frame, SubscriberType::BOTH);
+					frames.pop_front();
+
+					if (excess->size() > 0 && !frames.empty())
+					{
+						frames.front()->append_and_return_excess(excess);
+					}
+				}
 
 				if (total_triggers_processed >= total_triggers)
 				{
 					std::string finished = "finished";
 					zmq::message_t finished_msg(finished.size());
 					memcpy((void *)finished_msg.data(), finished.c_str(), finished.size());
-					
+
 					publisher->send(finished_msg, subject);
 				}
 			}
@@ -73,14 +81,14 @@ void ProcessSubject::on_notify()
 			total_triggers_processed += ad.stamps.size();
 			try
 			{
-				auto t1 = std::chrono::high_resolution_clock::now();
+				//auto t1 = std::chrono::high_resolution_clock::now();
 				auto results = ad.process(0, offset_bins);
-				auto t2 = std::chrono::high_resolution_clock::now();
-				auto dur = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1);
-				std::cout << "\tprocess time: " << dur.count() << "\n";
+				//auto t2 = std::chrono::high_resolution_clock::now();
+				//auto dur = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1);
+				//std::cout << "\tprocess time: " << dur.count() << "\n";
 
 				//auto t1_n = std::chrono::high_resolution_clock::now();
-				notify(results, SubscriberType::BOTH);
+				FramePublisher<segment_ptr>::notify(results, SubscriberType::BOTH);
 				//auto t2_n = std::chrono::high_resolution_clock::now();
 				//auto dur_n = std::chrono::duration_cast<std::chrono::milliseconds>(t2_n - t1_n);
 				//std::cout << "\t\tTIME TO PROCESS NOTIFY: " << dur_n.count() << "\n";
