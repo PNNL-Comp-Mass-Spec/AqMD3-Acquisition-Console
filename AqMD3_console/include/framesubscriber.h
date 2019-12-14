c627de74b1e93d2b4087605549477da2db0d4d31
Cameron Giberson <cameron.giberson@pnnl.gov>
2019-12-13 18:21:54 -0800

    make subscribes optionally reusable

--- AqMD3_console/include/framesubscriber.h
+++ AqMD3_console/include/framesubscriber.h
@@ -34,53 +34,68 @@ protected:
 	std::condition_variable sig;
 	std::mutex mut;
 
-	std::thread worker_handle;
+	std::future<void> worker_handle;
+
 	std::promise<void> has_completed;
 
-	bool run_detached;
+	bool reusable;
+
+	std::promise<void> reusable_notifier;
+	std::shared_future<void> is_reusable_stop;
+	bool is_running;
 
 public:
-	FrameSubscriber(bool run_detached = false)
+	FrameSubscriber(bool reusable = false)
 		: items()
 		, worker_handle()
-		, run_detached(run_detached)
+		, reusable(reusable)
+		, is_reusable_stop(reusable_notifier.get_future())
+		, is_running(false)
 	{}
 
 	virtual ~FrameSubscriber()
 	{
-		if(!run_detached)
-			worker_handle.join();
+		if (reusable)
+			reusable_notifier.set_value();
 	};
 
-	std::shared_future<void> setup(std::shared_future<void> stop)
+	std::shared_future<void> setup(std::shared_future<void> pub_stop)
 	{
-		worker_handle = std::thread([&, stop]()
+		if (!is_running)
 		{
-			while (stop.wait_for(std::chrono::seconds(0)) != std::future_status::ready || !items.empty())
+			auto stop = reusable ? is_reusable_stop : pub_stop;
+
+			worker_handle = std::async(std::launch::async, [&, stop]()
 			{
+				is_running = true;
+				while (stop.wait_for(std::chrono::seconds(0)) != std::future_status::ready || !items.empty())
 				{
-					std::unique_lock<std::mutex> lock(mut);
-					sig.wait_for(lock, std::chrono::milliseconds(10));
+					{
+						std::unique_lock<std::mutex> lock(mut);
+						sig.wait_for(lock, std::chrono::milliseconds(10));
+					}
+
+					try
+					{
+						on_notify();
+					}
+					catch (std::string ex)
+					{
+						std::cerr << ex << std::endl;
+					}
 				}
 
-				try
-				{
-					on_notify();
-				}
-				catch (std::string ex)
-				{
-					std::cerr << ex << std::endl;
-				}
-			}
+				on_completed();
+				has_completed.set_value();
+			});
 
-			on_completed();
-			has_completed.set_value();
-		});
+			if(!reusable)
+				return std::shared_future<void>(has_completed.get_future());
+		}
 
-		if (run_detached)
-			worker_handle.detach();
-
-		return std::shared_future<void>(has_completed.get_future());
+		auto promise = std::promise<void>();
+		promise.set_value();
+		return std::shared_future<void>(promise.get_future());
 	}
 
 	inline void update(T item)
