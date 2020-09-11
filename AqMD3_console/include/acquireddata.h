4667a2ab4ed40435e7f3bfa512ad2eee8cde1f5a
Cameron Giberson <cameron.giberson@pnnl.gov>
2020-09-11 15:42:31 -0700

    add comments to AcquiredData class and member clases.

--- AqMD3_console/include/acquireddata.h
+++ AqMD3_console/include/acquireddata.h
@@ -7,19 +7,31 @@
 #include <vector>
 #include <list>
 
+// Contains raw sample data, timestamp data, and gated-record data for both CST and CST-ZS acquisition.
+// For CST acquision, this class simply holds the raw sample data and cannot be used to extract samples
+// from elements.
 class AcquiredData {
 public:
+	// Gated record descriptor class used to calculate the first and last valid sample index, and the number of elements to process in the 
+	// flat array of elements.
 	class GateData {
 	private:
+		// This is the minimum size of a gated record on the SA220P and is used to calculate the sample index of the 
+		// first valid sample in a gated record and the position of the gated record in the the area after a trigger
+		// event when a sample can be recorded (also known as the "record").
 		int const processing_block_size = 8;
 
 	public:
+		// *_block_index member variables are the index in units of processing_block_size blocks of int32 elements within the record.
 		uint64_t const gate_start_block_index;
 		uint64_t const gate_stop_block_index;
 
+		// *_sample_pos member variables are the index of the first valid sample or one after the last valid sample within the processing_block_size 
+		// sized block at index gate_*_block index.
 		uint32_t const gate_start_sample_pos;
 		uint32_t const gate_stop_sample_pos;
 
+		// The total number of elements in the gated record. 
 		uint32_t const element_count;
 
 		GateData(uint64_t gate_start_block_index, uint32_t gate_start_sample_pos, uint64_t gate_stop_block_index,
@@ -42,6 +54,7 @@ public:
 
 	};
 
+	// Couples trigger timestamps data with gated record information.
 	class TriggerData {
 	public:
 		uint64_t const timestamp;
@@ -60,7 +73,9 @@ public:
 
 private:
 	std::shared_ptr<AcquisitionBuffer> samples_buffer;
-	int32_t const range_shift_constant = 32768; // [-2^15, 2^15 - 1] + 2^15 -> [0, 2^16 - 1]
+	
+	// Used to bring data into the unsigned int16 range. [-2^15, 2^15 - 1] + 2^15 -> [0, 2^16 - 1]
+	int32_t const range_shift_constant = 32768;
 
 public:
 	std::vector<TriggerData> const stamps;
@@ -73,6 +88,8 @@ public:
 		, samples_count(samples_count)
 	{}
 
+	// Processes the raw sample data and returns a vector of processed results equivalent to a row of scan data in a .uimf file. 
+	// Should not be called if the data was acquired without data-reduction enabled.
 	std::shared_ptr<std::vector<EncodedResult>> AcquiredData::process(int processing_scan_start_number, uint32_t post_trigger_samples) const;
 
 };
