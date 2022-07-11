23604bed914a6d536e47231bfb8d49dcfee3002e
gibe617 <cameron.giberson@pnnl.gov>
2022-07-11 11:41:17 -0700

    wrap all of the real-time display supporting subscriber in try-catch

--- AqMD3_console/src/zmqacquireddatasubscriber.cpp
+++ AqMD3_console/src/zmqacquireddatasubscriber.cpp
@@ -7,59 +7,73 @@
 #undef max
 #include "../include/message.pb.h"
 #include <stdexcept>
+#include <spdlog/spdlog.h>
 
 void ZmqAcquiredDataSubscriber::on_notify()
 {
-	while (!items.empty())
+	try
 	{
-		//std::cout << "VectorZmqWriterSubscriber unprocessed elements: " << items.size() << std::endl;
-
-		auto ad = items.front();
-		items.pop_front();
-
-		Message msg;
-
-		std::fill(data_vector.begin(), data_vector.end(), 0);
-
-		for (auto& er : *ad)
+		while (!items.empty())
 		{
-			msg.add_tic(er.tic);
-			msg.add_time_stamps(er.timestamp);
+			//std::cout << "VectorZmqWriterSubscriber unprocessed elements: " << items.size() << std::endl;
 
-			auto begin = std::begin(er.encoded_spectra);
-			auto end = std::end(er.encoded_spectra);
+			auto ad = items.front();
+			items.pop_front();
 
-			int index = 0;
-			for (auto val : er.encoded_spectra)
+			Message msg;
+
+			std::fill(data_vector.begin(), data_vector.end(), 0);
+
+			for (auto& er : *ad)
 			{
-				if (val < 0)
+				msg.add_tic(er.tic);
+				msg.add_time_stamps(er.timestamp);
+
+				auto begin = std::begin(er.encoded_spectra);
+				auto end = std::end(er.encoded_spectra);
+
+				int index = 0;
+				for (auto val : er.encoded_spectra)
 				{
-					index += (-1 * val);
-
-					if (index >= data_vector.size())
+					if (val < 0)
 					{
-						throw std::out_of_range("index oob error -> index: " + std::to_string(index));
+						index += (-1 * val);
+
+						if (index >= data_vector.size())
+						{
+							throw std::out_of_range("index oob error -> index: " + std::to_string(index));
+						}
+
+						continue;
 					}
-
-					continue;
+					data_vector[index++] += val;
 				}
-				data_vector[index++] += val;
 			}
+
+			*msg.mutable_mz() = { data_vector.begin(), data_vector.end() };
+
+			std::string msg_s;
+			msg.SerializeToString(&msg_s);
+
+			std::string compressed_msg_s;
+			snappy::Compress(msg_s.data(), msg_s.size(), &compressed_msg_s);
+
+			zmq::message_t to_send(compressed_msg_s.size());
+			memcpy((void*)to_send.data(), compressed_msg_s.c_str(), compressed_msg_s.size());
+
+			publisher->send(to_send, subject);
+
+			processed++;
 		}
-
-		*msg.mutable_mz() = { data_vector.begin(), data_vector.end() };
-
-		std::string msg_s;
-		msg.SerializeToString(&msg_s);
-
-		std::string compressed_msg_s;
-		snappy::Compress(msg_s.data(), msg_s.size(), &compressed_msg_s);
-
-		zmq::message_t to_send(compressed_msg_s.size());
-		memcpy((void *)to_send.data(), compressed_msg_s.c_str(), compressed_msg_s.size());
-
-		publisher->send(to_send, subject);
-
-		processed++;
+	}
+	catch (const std::exception& ex)
+	{
+		spdlog::error("Error occured when processing ZMQ data: " + std::string(ex.what()));
+		spdlog::error("ZMQ unprocessed elements: " + std::to_string(items.size()));
+	}
+	catch (...)
+	{
+		spdlog::error("Unknown error occured when processing ZMQ data");
+		spdlog::error("ZMQ unprocessed elements: " + std::to_string(items.size()));
 	}
 }
