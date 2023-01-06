eb7b1756eda858dca24873062d7fb3b8eb86828f
gibe617 <cameron.giberson@pnnl.gov>
2023-01-06 14:56:34 -0800

    add buffer pool class

---
+++ libaqmd3/include/libaqmd3/acquisitionbufferpool.h
@@ -0,0 +1,34 @@
+#ifndef ACQUISITION_BUFFER_POOL_H
+#define ACQUISITION_BUFFER_POOL_H
+
+#include "acquisitionbuffer.h"
+
+class AcquisitionBufferPool
+{
+private:
+	std::vector<std::shared_ptr<AcquisitionBuffer>> samples_buffer_pool;
+
+	uint64_t buffer_takes;
+	uint64_t buffer_max_count;
+	uint64_t buffer_initial_count;
+
+public:
+	AcquisitionBufferPool(uint64_t triggers_per_read, uint64_t samples_per_trigger, uint64_t buffer_initial_count, uint64_t buffer_max_count)
+		: buffer_takes(0)
+		, buffer_max_count(buffer_max_count)
+		, buffer_initial_count(buffer_initial_count)
+	{
+		for (int i = 0; i < buffer_initial_count; i++)
+		{
+			samples_buffer_pool.push_back(std::make_shared<AcquisitionBuffer>(i, triggers_per_read, samples_per_trigger));
+		}
+	}
+
+	size_t get_available_buffers();
+
+protected:
+	std::shared_ptr<AcquisitionBuffer> get_buffer();
+
+};
+
+#endif //ACQUISITION_BUFFER_POOL_H
---
+++ libaqmd3/src/acquisitionbufferpool.cpp
@@ -0,0 +1,33 @@
+#include "../include/libaqmd3/acquisitionbufferpool.h"
+
+size_t AcquisitionBufferPool::get_available_buffers()
+{
+	size_t count = buffer_max_count;
+	for (const auto& buf : samples_buffer_pool)
+	{
+		if (buf.use_count() >= 2)
+			--count;
+	}
+
+	return count;
+}
+
+std::shared_ptr<AcquisitionBuffer> AcquisitionBufferPool::get_buffer()
+{
+	int total = samples_buffer_pool.size();
+	for (int i = 0; i < total; i++)
+	{
+		int index = (buffer_takes + i) % total;
+		//TODO .use_count() not reliable, find other way
+		if (samples_buffer_pool[index].use_count() == 1)	
+		{
+			buffer_takes++;
+			samples_buffer_pool[index]->reset();
+			return samples_buffer_pool[index];
+		}
+	}
+
+	samples_buffer_pool.push_back(std::make_shared<AcquisitionBuffer>(total, samples_buffer_size));
+	buffer_takes++;
+	return samples_buffer_pool[total];
+}
